5b1e6b4a1d1cecf807fa854d7567793599bc009c
dania-afridi <dania.awais33@gmail.com>
2022-10-04 23:37:52 +0200

    updated chart file

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,14 +1,27 @@
+import '../models/transactions.dart';
 import 'package:flutter/material.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({super.key});
+  //const Chart({super.key});
+
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get weeklyTransactionValues {
+    return List.generate(7, (index) {
+      return {'day': 'T', 'amount': 9.99};
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(),
+      child: Row(
+        children: <Widget>[],
+      ),
     );
   }
 }
