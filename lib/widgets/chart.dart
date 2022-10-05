17b6d50ef15ce942d59142a45fc7f1292bba261f
dania-afridi <dania.awais33@gmail.com>
2022-10-05 09:03:19 +0200

    added chart-bar in chart file

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -3,6 +3,7 @@
 import '../models/transactions.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   //const Chart({super.key});
@@ -26,7 +27,6 @@ class Chart extends StatelessWidget {
           totalAmount += recentTransactions[i].amount!;
         }
       }
-
       print(DateFormat.E().format(weekDay));
 
       return {
@@ -36,16 +36,36 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return weeklyTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print(weeklyTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: weeklyTransactionValues.map((data) {
-          return Text('${data['day']} : ${data['amount']}  ');
-        }).toList(),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: weeklyTransactionValues.map((data) {
+            return Flexible(
+              //flex: 1,
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['day'] as String,
+                data['amount'] as double,
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
